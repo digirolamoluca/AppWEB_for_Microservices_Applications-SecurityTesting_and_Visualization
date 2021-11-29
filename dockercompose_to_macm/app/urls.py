ca86fdf880719cbaeedba031d42b5df438e3f0a9
Luca <90553744+digirolamoluca@users.noreply.github.com>
2021-11-29 12:38:22 +0100

    Add files via upload

---
+++ dockercompose_to_macm/app/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.app),
+    path('customize', views.customize),
+    #path('viewmacm', views.viewmacm),
+]
+
---
+++ dockercompose_to_macm/app/views.py
@@ -0,0 +1,51 @@
+import os
+from django.core.files.storage import FileSystemStorage
+from django.http import HttpResponse
+from django.shortcuts import render
+from . import macm
+import datetime
+
+
+
+# Create your views here.
+def app(request):
+    #print(request.POST)
+    return render(request, 'app/base.html')
+
+def generate_macm(request):
+
+    now = datetime.datetime.now()
+    x=now.strftime('%d-%m-%YT%H:%M:%S')
+
+    payload=request.POST
+    #print(payload)
+    file=request.FILES['file']
+
+    fs = FileSystemStorage(location="./docker_compose")
+
+    relativePath="./docker_compose/"+file.name
+    if os.path.exists(relativePath):
+        os.remove(relativePath)
+    filename = fs.save(file.name, file)
+
+    if request.method == 'POST':
+        macm.dockercompose_WriteTo_macmFile(relativePath,payload['app_id'],payload['application_name'])
+    else:
+        print("POST ERROR")
+
+
+    response = HttpResponse(open("./"+payload['application_name']+".macm", 'rb').read())
+    response['Content-Type'] = 'text/plain'
+    response['Content-Disposition'] = 'attachment; filename='+payload['application_name']+'_'+x+'.macm'
+    return response
+
+
+
+def customize(request):
+#    if request.method == 'GET':
+#        print('hello')
+    return render(request, 'app/customize.html')
+
+#def viewmacm(request):
+#    content = open("./"+request.POST['application_name']+".macm").read()
+#    return HttpResponse(content, content_type='text/plain')
