2bf06a946c04fa3d51bef4f188198b90125ee008
Luca <90553744+digirolamoluca@users.noreply.github.com>
2022-03-17 17:46:11 +0100

    Update wsgi.py

--- dockercompose_to_macm/wsgi.py
+++ dockercompose_to_macm/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dockercompose_to_macm.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AppWEB_for_Microservices_Applications-SecurityTesting_and_Visualization.settings')
 
 application = get_wsgi_application()
